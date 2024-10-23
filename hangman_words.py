7f2b8de6a6debd7a9cd99a08c3f008360e99c94f
SaiPraneeth-E <edupulapatisaipraneeth12345@gmail.com>
2024-10-23 23:03:08 +0530

    Add files via upload

---
+++ hangman_words.py
@@ -0,0 +1,95 @@
+stages = ['''
+    +----+
+    |    |
+    O    |
+   /|\   |
+   / \   |
+         |
+==============''',
+    '''
+    +----+
+    |    |
+    O    |
+   /|\   |
+   /     |
+         |
+==============''','''
++----+
+    |    |
+    O    |
+   /|\   |
+         |
+         |
+==============''','''
++----+
+    |    |
+    O    |
+   /|    |     
+         |
+         |
+==============''',
+'''
++----+
+    |    |
+    O    |
+    |    |     
+         |
+         |
+==============''',
+'''
++----+
+    |    |
+    O    |
+         |     
+         |
+         |
+==============''',
+'''
++----+
+    |    |
+         |
+         |     
+         |
+         |
+=============='''
+          ]
+word_list =[
+    "AFGHANISTAN", "ALBANIA", "ALGERIA", "ANDORRA", "ANGOLA",
+    "ANTIGUA AND BARBUDA", "ARGENTINA", "ARMENIA", "AUSTRALIA",
+    "AUSTRIA", "AUSTRIAN EMPIRE*", "AZERBAIJAN", "BADEN*",
+    "BAHAMAS, THE", "BAHRAIN", "BANGLADESH", "BARBADOS",
+    "BAVARIA*", "BELARUS", "BELGIUM", "BELIZE", "BENIN",
+    "BOLIVIA", "BOSNIA AND HERZEGOVINA", "BOTSWANA", "BRAZIL",
+    "BRUNEI", "BRUNSWICK AND LÜNEBURG*", "BULGARIA", "BURKINA FASO",
+    "BURMA", "BURUNDI", "CABO VERDE", "CAMBODIA", "CAMEROON",
+    "CANADA", "CAYMAN ISLANDS, THE", "CENTRAL AFRICAN REPUBLIC",
+    "CENTRAL AMERICAN FEDERATION*", "CHAD", "CHILE", "CHINA",
+    "COLOMBIA", "COMOROS", "CONGO FREE STATE, THE*", "COOK ISLANDS",
+    "COSTA RICA", "COTE D’IVOIRE (IVORY COAST)", "CROATIA", "CUBA",
+    "CYPRUS", "CZECHIA", "CZECHOSLOVAKIA*", "DEMOCRATIC REPUBLIC OF THE CONGO",
+    "DENMARK", "DJIBOUTI", "DOMINICA", "DOMINICAN REPUBLIC",
+    "DUCHY OF PARMA, THE*", "EAST GERMANY",
+    "ECUADOR", "EGYPT", "EL SALVADOR", "EQUATORIAL GUINEA", "ERITREA",
+    "ESTONIA", "ESWATINI", "ETHIOPIA", "FEDERAL GOVERNMENT OF GERMANY",
+    "FIJI", "FINLAND", "FRANCE", "GABON", "GAMBIA, THE", "GEORGIA",
+    "GERMANY", "GHANA", "GRAND DUCHY OF TUSCANY, THE*", "GREECE",
+    "GRENADA", "GUATEMALA", "GUINEA", "GUINEA-BISSAU", "GUYANA",
+    "HAITI", "HANOVER*", "HANSEATIC REPUBLICS*", "HAWAII*",
+    "HESSE*", "HOLY SEE", "HONDURAS", "HUNGARY", "ICELAND",
+    "INDIA", "INDONESIA", "IRAN", "IRAQ", "IRELAND", "ISRAEL",
+    "ITALY", "JAMAICA", "JAPAN", "JORDAN", "KAZAKHSTAN", "KENYA",
+    "KINGDOM OF SERBIA/YUGOSLAVIA*", "KIRIBATI", "KOREA", "KOSOVO",
+    "KUWAIT", "KYRGYZSTAN", "LAOS", "LATVIA", "LEBANON", "LESOTHO",
+    "LEW CHEW", "LIBERIA", "LIBYA", "LIECHTENSTEIN",
+    "LITHUANIA", "LUXEMBOURG"
+
+]
+
+logo = '''                                          
+| | | |                                         
+| |_| | __ _ _ __   __ _  _ __ __   ____ ____  
+| '_' |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+| | | |\__,_|_| |_|\__, | | |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/        '''
